d0097bfdd2ead7b282011c4826c9278b4ef3df90
yum-horok <ehue1118@gmail.com>
2025-03-03 01:16:48 +0900

    완.

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,4 +120,17 @@
 			speed: 1000
 		});
 
+function showTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    document.querySelectorAll('.tab-button').forEach(button => {
+        button.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+    document.querySelector(`[onclick="showTab('${tabId}')"]`).classList.add('active');
+}
+
 })(jQuery);
